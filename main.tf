10af85f0708932435eeb60903420f9065fe832ef
Kaline Cecília Constantino <137835548+Kaline-Constantino@users.noreply.github.com>
2024-07-23 13:39:31 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -26,6 +26,13 @@ resource "azurerm_subnet" "subnet" {
   address_prefixes     = ["10.0.1.0/24"]
 }
 
+resource "azurerm_public_ip" "public_ip" {
+  name                = "minsait_teste_public_ip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
+}
+
 resource "azurerm_network_interface" "nic" {
   name                = "minsait_teste_nic"
   location            = azurerm_resource_group.rg.location
@@ -39,19 +46,13 @@ resource "azurerm_network_interface" "nic" {
   }
 }
 
-resource "azurerm_public_ip" "public_ip" {
-  name                = "minsait_teste_public_ip"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Dynamic"
-}
-
 resource "azurerm_virtual_machine" "vm" {
-  name                  = "minsait_teste_vm"
-  location              = azurerm_resource_group.rg.location
-  resource_group_name   = azurerm_resource_group.rg.name
+  depends_on           = [azurerm_public_ip.public_ip]
+  name                 = "minsait_teste_vm"
+  location             = azurerm_resource_group.rg.location
+  resource_group_name  = azurerm_resource_group.rg.name
   network_interface_ids = [azurerm_network_interface.nic.id]
-  vm_size               = "Standard_DS1_v2"
+  vm_size              = "Standard_DS1_v2"
 
   os_profile {
     computer_name  = "hostname"
